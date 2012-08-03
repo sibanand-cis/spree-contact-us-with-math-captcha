a80cc4562fb6920ab43106340efbcc32cc8a4332
Chris Edwards <chris@leopipadesigns.co.uk>
2012-08-03 14:07:49 +0200

    Changed mailer to fix auth errors

--- app/mailers/spree/inquiry_mailer.rb
+++ app/mailers/spree/inquiry_mailer.rb
@@ -4,7 +4,7 @@ class Spree::InquiryMailer < ActionMailer::Base
   	recipients = Spree::MailMethod.current.preferred_mail_bcc.split()
   	recipients = Spree::MailMethod.current.preferred_intercept_email unless Spree::MailMethod.current.preferred_intercept_email.blank?
     @inquiry = inquiry
-    mail(:from => inquiry.email, :to => recipients, :subject => "#{Spree::Config[:site_name]} : #{I18n.t(:contact_form)}")
+    mail(:from => recipients, :to => recipients, :subject => "#{Spree::Config[:site_name]} : #{I18n.t(:contact_form)}")
   end
 
 end
